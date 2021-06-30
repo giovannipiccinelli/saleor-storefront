896c76bcbdea315de558594b142409eaa0b9f2c3
IvanovJenyIT <jenyit@ya.ru>
2021-06-30 13:07:43 +0300

    Fixed smal bugs

--- src/@next/components/organisms/PersonalizationFormModal/PersonalizationFormModal.tsx
+++ src/@next/components/organisms/PersonalizationFormModal/PersonalizationFormModal.tsx
@@ -36,10 +36,7 @@ export const PersonalizationFormModal: React.FC<IProps> = ({
         {...props}
         {...{ errors }}
         formId={formId}
-        handleSubmit={data => {
-          console.log("data", data);
-          hideModal();
-        }}
+        handleSubmit={() => hideModal()}
       />
     </Modal>
   );
--- src/views/Collection/Page.tsx
+++ src/views/Collection/Page.tsx
@@ -1,4 +1,3 @@
-import classNames from "classnames";
 import "./scss/index.scss";
 
 import {
